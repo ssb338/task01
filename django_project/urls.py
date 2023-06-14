d9d40584049cca738ce9f5e0aa597441592ce785
ssb338 <shivanishekhar338@gmail.com>
2023-06-15 01:15:44 +0530

    Prod Ready

--- django_project/urls.py
+++ django_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("shops/", include("shops.urls"), namespace="shops"),
+    path("shops/", include("shops.urls")),
 ]
